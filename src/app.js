93e13aa9bc1b504edd3bc522be9501c0891a9533
nitish bahuguna <webwork150@gmail.com>
2025-09-18 00:16:07 +0530

    setup complete

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// setting up express with right middleware
+
+import express from 'express';
+
+const app = express();
+
+
+app.get('/' , (req ,res)=>{
+    res.status(200).send('Hello world!');
+})
+
+export default app;
